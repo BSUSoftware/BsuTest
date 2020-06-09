e8d1e7af516396153c89ecd419c5bffedcb2c1a0
espn1 <espn1@N00087>
2020-06-09 14:46:49 +0100

    Setup JediController for JediAcademy.Back.Api project Add JediAcademy.Back.Domain project

---
+++ JediAcademy.Back.Domain/Entities/Jedi.cs
@@ -0,0 +1,11 @@
+﻿namespace JediAcademy.Back.Domain.Entities
+{
+    public class Jedi
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Height { get; set; }
+        public int Mass { get; set; }
+        public string Species { get; set; }
+    }
+}
